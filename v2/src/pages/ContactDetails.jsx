2102350ca740d83af33421299c850a04af71948e
Jacob Menachem <dorlabor1@gmail.com>
2022-09-10 15:11:09 +0300

    fix: regular component to arrow function with useEffect only

--- v2/src/pages/ContactDetails.jsx
+++ v2/src/pages/ContactDetails.jsx
@@ -1,41 +1,47 @@
-import { Component } from 'react'
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { loadContact } from '../store/actions/contactActions'
 import { TransferFund } from '../cmps/TransferFund'
 import { MoveList } from '../cmps/MoveList'
-class _ContactDetails extends Component {
-  async componentDidMount() {
-    const contactId = this.props.match.params.id
-    await this.props.loadContact(contactId)
+
+const _ContactDetails = (props) => {
+  let params = useParams()
+
+  const { contact, onTransferCoins, onChangefunds, funds, loggedInUser } = props
+
+  useEffect(() => {
+    loadContact(params.id)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params.id])
+
+  const loadContact = async (contactId) => {
+    await props.loadContact(contactId)
   }
 
-  onBack = () => {
-    this.props.history.push('/contacts')
-    // this.props.history.goBack()
+  const onBack = () => {
+    props.history.push('/contacts')
+    // props.history.goBack()
   }
 
-  render() {
-    const { contact, onTransferCoins, onChangefunds, funds, loggedInUser } = this.props
-
-    if (!contact || !loggedInUser) return <div>Loading...</div>
-    const moves = loggedInUser.moves.filter((move) => move.to._id === contact._id)
-    return (
-      <div className="contact-details">
-        {Object.entries(contact).map(([key, value]) => {
-          return (
-            <section key={key}>
-              <h3>{key === '_id' ? '' : `${key.charAt(0).toUpperCase() + key.slice(1)}: ${value}`}</h3>
-            </section>
-          )
-        })}
-        <img src={`https://robohash.org/${contact._id}`} alt="" />
-        <button onClick={this.onBack}>Back</button>
-        {/* <Link to="/contact/r3">Next Contact</Link> */}
-        <TransferFund contact={contact} onTransferCoins={onTransferCoins} onChange={onChangefunds} value={funds} />
-        <MoveList moves={moves} />
-      </div>
-    )
-  }
+  if (!contact || !loggedInUser) return <div>Loading...</div>
+  const moves = loggedInUser.moves.filter((move) => move.to._id === contact._id)
+  return (
+    <div className="contact-details">
+      {Object.entries(contact).map(([key, value]) => {
+        return (
+          <section key={key}>
+            <h3>{key === '_id' ? '' : `${key.charAt(0).toUpperCase() + key.slice(1)}: ${value}`}</h3>
+          </section>
+        )
+      })}
+      <img src={`https://robohash.org/${contact._id}`} alt="" />
+      <button onClick={onBack}>Back</button>
+      {/* <Link to="/contact/r3">Next Contact</Link> */}
+      <TransferFund contact={contact} onTransferCoins={onTransferCoins} onChange={onChangefunds} value={funds} />
+      <MoveList moves={moves} />
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
--- v2/src/pages/ContactEdit.jsx
+++ v2/src/pages/ContactEdit.jsx
@@ -1,27 +1,32 @@
-import { Component } from 'react'
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { loadContact, loadContacts } from '../store/actions/contactActions'
 import { FormTemplate } from '../cmps/FormTemplate'
 
-class _ContactEdit extends Component {
-  async componentDidMount() {
-    await this.props.loadContacts()
-    const contactId = this.props.match.params.id
-    contactId ? await this.props.loadContact(contactId) : await this.props.loadContact()
+const _ContactEdit = (props) => {
+  const params = useParams()
+
+  const loadContact = async (contactId) => {
+    await props.loadContacts()
+    contactId ? await props.loadContact(contactId) : await props.loadContact()
   }
 
-  render() {
-    const { contact, onChange, onSubmitContact } = this.props
-    if (!contact) return <div>Loading...</div>
-    const selectedFormFields = ['name', 'email', 'phone']
-    const onSubmit = { action: (e) => onSubmitContact(e), forHtml: 'Save' }
-    return (
-      <section className="contact-edit">
-        <h1>{contact._id ? 'Edit' : 'Add'} Contact</h1>
-        <FormTemplate value={contact} selectedFormFields={selectedFormFields} onSubmit={onSubmit} onChange={onChange} />
-      </section>
-    )
-  }
+  useEffect(() => {
+    loadContact(params.id)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params.id])
+
+  const { contact, onChange, onSubmitContact } = props
+  if (!contact) return <div>Loading...</div>
+  const selectedFormFields = ['name', 'email', 'phone']
+  const onSubmit = { action: (e) => onSubmitContact(e), forHtml: 'Save' }
+  return (
+    <section className="contact-edit">
+      <h1>{contact._id ? 'Edit' : 'Add'} Contact</h1>
+      <FormTemplate value={contact} selectedFormFields={selectedFormFields} onSubmit={onSubmit} onChange={onChange} />
+    </section>
+  )
 }
 
 const mapStateToProps = (state) => {
